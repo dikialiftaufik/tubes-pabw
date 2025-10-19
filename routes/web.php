e322f6fa772c6de876746f447d7394b7b9af64a3
dikialiftaufik <dikialift13@gmail.com>
2025-10-20 04:28:54 +0700

    fix auth route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AdminMenuController;
 use App\Http\Controllers\AdminFeedbackController;
