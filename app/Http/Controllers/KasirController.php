53c9450c1800f6dda7607530486bae5fbe8cced3
egafiandra <egafiandrapratama@gmail.com>
2025-10-20 10:37:49 +0700

    tampilan kasir

--- app/Http/Controllers/KasirController.php
+++ app/Http/Controllers/KasirController.php
@@ -9,14 +9,30 @@ class KasirController extends Controller
     
     public function index()
     {
-        return view('Kasir.index');
+        
+        $menus = [
+            ['nama' => 'Sate Ayam', 'stok' => 10, 'gambar' => 'img/menu/sate-ayam.jpg'],
+            ['nama' => 'Sate Sapi', 'stok' => 8, 'gambar' => 'img/menu/sate-sapi.jpg'],
+            ['nama' => 'Sate Kambing', 'stok' => 5, 'gambar' => 'img/menu/sate-kambing.jpg'],
+            ['nama' => 'Tongseng Ayam', 'stok' => 7, 'gambar' => 'img/menu/tongseng-ayam.jpg'],
+            ['nama' => 'Tongseng Sapi', 'stok' => 6, 'gambar' => 'img/menu/tongseng-sapi.jpg'],
+            ['nama' => 'Tongseng Kambing', 'stok' => 4, 'gambar' => 'img/menu/tongseng-kambing.jpg'],
+        ];
 
+        
+        $pesanan = [
+            ['nama' => 'Rina', 'menu' => 'Sate Ayam', 'jumlah' => 2, 'status' => 'Sedang Dibuat'],
+            ['nama' => 'Budi', 'menu' => 'Tongseng Sapi', 'jumlah' => 1, 'status' => 'Selesai'],
+            ['nama' => 'Dewi', 'menu' => 'Sate Kambing', 'jumlah' => 3, 'status' => 'Sedang Dibuat'],
+        ];
+
+        // Kirim data ke view
+        return view('kasir.index', compact('menus', 'pesanan'));
     }
 
     
     public function updateStok(Request $request)
     {
-
         return redirect()->back()->with('success', 'Stok berhasil diperbarui!');
     }
 }
